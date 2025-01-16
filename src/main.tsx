e42217fe5465b1dc5281f42a43b330d0533d6372
Tim Calhoun <timothydcalhoun@gmail.com>
2025-01-16 15:33:25 -0600

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -6,9 +5,7 @@ import {store} from '../src/store/store.ts'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
       <Provider store={store}>
           <App />
       </Provider>
-  /*</StrictMode>*/
 )
